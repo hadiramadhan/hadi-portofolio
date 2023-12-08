983b46f04c18a982ac9324fa020088d472896df2
HadiRamadhan <151528923+HadiRamadhan@users.noreply.github.com>
2023-12-09 04:18:02 +0700

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+  // ...
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+      // tambahkan path halaman lain jika diperlukan
+    };
+  },
+};
